460b7527cea7110063c3134c8003cf3346ccb0b4
seankoko98 <78056052+seankoko98@users.noreply.github.com>
2021-02-11 21:34:55 -0800

    added files

--- sketch.js
+++ sketch.js
@@ -9,32 +9,51 @@
 	* Shows use of subdirectories
 	
 */
+import calm from "./assets/calm.png"
+import cheerful from "./assets/cheerful.png"
+import comforting from "./assets/comforting.png"
 
 // Global variable that we will use in all the functions
 var img; 
 
+// Run the program
+setup();
+
 // preload() will execture before setup()
 function preload() {
-	print("SimpleImageDisplay: preload()");
-
-  img = loadImage('assets/figure16.png'); // Load the image
+  	print("SimpleImageDisplay: preload()");
+  	let imgs = ['assets/figure16.png', calm, cheerful, comforting].map(imgUrl => loadImage(imgUrl)); // Load the image
+  	return imgs
 }
 
 // set image mode to center, so that it is center-drawn
 function setup() {
+	const imgs = preload()
 	print("SimpleImageDisplay: setup()");
 	print(img);
 
 	createCanvas(800, 600);
+	imgs.forEach(img => imageMode(CENTER));
 
-	imageMode(CENTER);
+	draw(imgs);
 }
 
 // draw image in the center of the screen
-function draw() {
+function draw(imgs = []) {
 	// draw the background
 	background(128);
 		
   	// Displays the image at center point - add param 3 and 4 fo width and height
-  	image(img, width/2, height/2);
+  	imgs.forEach(img => image(img, width/2, height/2));
 }
+
+function iter(list){
+	for (let item of list) {
+		console.log(item)
+	}
+
+	for (let i = 0; i < list.length; i++) {
+		console.log(list[i])
+	}
+}
+
