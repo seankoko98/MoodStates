310053f4a8f6c52e80b2638cc1ab77504169e8c7
seankoko98 <78056052+seankoko98@users.noreply.github.com>
2021-02-14 20:10:09 -0800

    deleted and replaced files to start over

--- sketch.js
+++ sketch.js
@@ -1,45 +1,135 @@
-/*
-	Simple image display example
+/***********************************************************************************
+	SimpleStateMachine - TEMPLATE
 	by Scott Kildall
 
-	Displays an image, centered.
+	Template:
 
-	* Use to show how to launch a localhost from terminal/command prompt
-	* Shows preload() command
-	* Shows use of subdirectories
-	
-*/
-// Global variable that we will use in all the functions
-var img; 
+	(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
+	(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
+	(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
 
-// Run the program
-setup();
+	Also start your localhost before running this, otherwise no PNGs will display
 
-// preload() will execture before setup()
+------------------------------------------------------------------------------------
+	The way it works — you don't need to know this for the template use
+	* array of images gets loaded at startup
+	* drawFunction is a VARIABLE that points to a function varible name
+	* drawOne(), drawTwo(), etc. are set to be functions.
+	* the the keys 1-5 will change the drawFunction variable
+
+------------------------------------------------------------------------------------
+	Notes:
+	- a more advanced state machine with use array-indexing for each of
+		images the draw functions, but this is just for illustrative purposes
+
+	- even more advanced will be to put the draw functions into an array, would
+		be helpful for randomizing, go to the next function, etc
+
+	- next step after that would be to put interfaces into an array that maps to
+		the functions
+
+
+***********************************************************************************/
+
+// Array of images
+var images = [];
+
+// variable that is a function 
+var drawFunction;
+
+// offset from bottom of screen
+var gTextOffset = 20;
+
+// load all images into an array
 function preload() {
-  	print("SimpleImageDisplay: preload()");
-  	let imgs = ['assets/figure16.png',"./assets/calm.png", "./assets/cheerful.png", "./assets/comforting.png"].map(imgUrl => loadImage(imgUrl)); // Load the image
-  	return imgs
+  images[0] = loadImage('assets/one.png');
+  images[1] = loadImage('assets/two.png');
+  images[2] = loadImage('assets/three.png');
+  images[3] = loadImage('assets/four.png');
+  images[4] = loadImage('assets/five.png');
 }
 
-// set image mode to center, so that it is center-drawn
+// Center drawing, drawFunction will be one for default
 function setup() {
-	const imgs = preload()
-	print("SimpleImageDisplay: setup()");
-	print(img);
+  createCanvas(windowWidth, windowHeight);
 
-	createCanvas(800, 600);
-	imgs.forEach(img => imageMode(CENTER));
+  // Center our drawing objects
+  imageMode(CENTER);
+  textAlign(CENTER);
+  textSize(24);
 
-	draw(imgs);
+  // set to one for startup
+  drawFunction = drawOne;
 }
 
-// draw image in the center of the screen
-function draw(imgs = []) {
-	// draw the background
-	background(128);
-		
-  	// Displays the image at center point - add param 3 and 4 fo width and height
-  	imgs.forEach(img => image(img, width/2, height/2));
+// Very simple, sets the background color and calls your state machine function
+function draw() {
+  background(192);
+
+  // will call your state machine function
+  drawFunction();
 }
 
+//========= TEMPLATE: modify these functions, INSIDE the function blocks only =========
+
+//-- drawOne() will draw the image at index 0 from the array
+drawOne = function() {
+   image(images[0],width/2, height/2);
+
+   fill(0,0,0);
+   text("State One", width/2, height - gTextOffset);
+}
+
+//-- drawTwo() will draw the image at index 1 from the array
+drawTwo = function() {
+   image(images[1],width/2, height/2);
+
+   fill(240,120,0);
+   text("State Two", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 2 from the array
+drawThree = function() {
+   image(images[2],width/2, height/2);
+
+   fill(40,230,120);
+   text("State Three", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 3 from the array
+drawFour = function() {
+   image(images[3],width/2, height/2);
+
+   fill(255,255,178);
+   text("State Four", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 4 from the array
+drawFive = function() {
+   image(images[4],width/2, height/2);
+
+   fill(230,50,50);
+   text("State Five", width/2, height - gTextOffset);
+}
+
+
+//========= TEMPLATE: add or change interface functions, as you like =========
+
+// Change the drawFunction variable based on your interaction
+function keyTyped() {
+  if( key === '1' ) {
+  	drawFunction = drawOne;
+  }
+  else if( key === '2' ) {
+  	drawFunction = drawTwo;
+  }
+  else if( key === '3' ) {
+  	drawFunction = drawThree;
+  }
+  else if( key === '4' ) {
+  	drawFunction = drawFour;
+  }
+  else if( key === '5' ) {
+  	drawFunction = drawFive;
+  }
+}
